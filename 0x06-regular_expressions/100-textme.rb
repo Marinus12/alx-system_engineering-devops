a52f2272613bb392c84c3eb6165c3d066d7d3937
Ukewuihe Marinus <dubemarinus@gmail.com>
2024-03-22 15:27:01 -0700

    0x0A. Configuration management

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,20 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+
+def extract_info(log_line)
+	match = log_line.match(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
+	if match
+		sender = match[1]
+		receiver = match[2]
+		flags = match[3]
+		return "#{sender},#{receiver},#{flags}"
+	else
+		return nil
+	end
+end
+
+ARGF.each do |line|
+	if line.include?("Receive SMS") || line.include?("Sent SMS")
+		info = extract_info(line)
+		puts info if info
+	end
+end
